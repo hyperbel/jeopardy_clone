ef6b88710fd971df961febda0d9ef190ac6516f0
hyperbel <thomaszimmer524@gmail.com>
2022-10-19 14:21:03 +0200

    don't reinstall go & sqlite

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-sudo pacman -Sy go sqlite3
+rm database.db
+pacman -Qs go >> /dev/null
+if [ $? -eq 1 ]
+then
+  sudo pacman -Sy go
+fi
+
+pacman -Qs sqlite3 >> /dev/null
+
+if [ $? -eq 1 ]
+then
+  sudo pacman -Sy go sqlite3
+fi
+
 go get -u
 cat database.sql | sqlite3 database.db
 go run .
